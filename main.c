6dcf39b3d00b9865d4e642a2701994aafb52cb6f
pimplod <p_implod@yahoo.com>
2018-03-27 03:13:50 -0700

    combined repeated segments of different functions into one callable function

--- main.c
+++ main.c
@@ -26,7 +26,6 @@
 #include <stdlib.h>
 
 #include "system.h"
-//#include "user.h"
 
 
 void TimerOn(void);
@@ -45,87 +44,88 @@ void main(void) {
     InitSystem();
     // Initialize Buttons, Encoder, Buzzer
     InitHardware();
-    //Open Communication with Display Driver
-    InitDisplay();
     //Restart all programmed interrupts
     ENABLE_INTERRUPTS();
     //Start main loop
     while (1) {
-
         switch (mainState) {
-
             case POWER_ON:
-                if (flag.ready) {
-                    flag.ready = false;
+                if (flag.stateChange) {
+                    flag.stateChange = false;
                     ScrollMessage("READY");
                     DisplayMsg("RDY");
                     pCount = t3Count + 400;
-                }else if (pCount < t3Count){
-                    flag.ready = true;
-                }
-                
-                if(signal.scrollbreak){
-                    signal.scrollbreak = false;
-                     flag.ready = true;
-
-                }
-                break;
-                
-            case CHOOSE_OP:
-                ChooseOperation();
-                break;
-                
-            case SET_TIMER:
-                SetTimer();
-                break;
-
-            case READY:
-                signal.blink_disp = true;
-                break;
-
-            case ON_TIMED:
-                TimerOn();
-                break;
-
-            case TIMER_OVER:
-                signal.blink_disp = true;
-                signal.buzzer = true;
-                break;
-
-            case STOP_CALLED:
-                if (flag.ready) {
-                    flag.ready = false;
-                    ScrollMessage("STOPPED");
-                    pCount = tmrCount + 400;
-                }else if (pCount < tmrCount) {
-                    flag.ready = true;
+                }else if (pCount < t3Count) {
+                    flag.stateChange = true;
                 }
 
                 if (signal.scrollbreak) {
                     signal.scrollbreak = false;
-                    flag.ready = true;
+                    flag.stateChange = true;
+
                 }
                 break;
-                
-            case ON_NT:
+            case CHOOSE_OP:
+                ChooseOperation();
+                break;
+            case SET_TIMER:
+                SetTimer();
+                break;
+            case READY:
+                signal.blink_disp = true;
+                break;
+            case ON_TIMED:
+                TimerOn();
+                break;
+            case TIMER_OVER:
+                signal.blink_disp = true;
+                signal.buzzer = true;
+                break;
+            case STOP_CALLED:
+                if (flag.stateChange) {
+                    flag.stateChange = false;
+                    ScrollMessage("STOPPED");
+                    pCount = tmrCount + 400;
+                }else if (pCount < tmrCount) {
+                    flag.stateChange = true;
+                }
+
+                if (signal.scrollbreak) {
+                    signal.scrollbreak = false;
+                    ClearButtons();
+                    flag.stateChange = true;
+                }
+                break;
+            case TURN_ON:
                 //flag set only when state changes 
-                if (flag.no_timer) {
-                    flag.no_timer = false;                              
-                    ClearLatched();               
+                if (flag.stateChange) {
+                    flag.stateChange = false;
+                    ClearLatched();
                     //set on flag for NoTimer() function
                     flag.on = true;
                 }/*if(signal.no_timer)*/
-                
-                NoTimer();              
+                NoTimer();
+                break;
+            case TURN_OFF:
+                if (flag.stateChange) {
+                    flag.stateChange = false;
+                    signal.blink_disp = true;
+                    signal.buzzer = true;
+                    SaveTime(timerValue);
+                }
+                break;
+            case ERASE_DATA:
+                EraseData();
                 break;
             default:
                 ChangeState(POWER_ON);
                 break;
         }
-        
         //Check Button Flags, once every main loop
         HandleButtons();
-
+        
+        if (flag.latched)
+            ClearLatched();
     }//while(1)
 }
 
@@ -155,19 +155,21 @@ void TimerOn(void) {
 
 }
 
-void NoTimer(void){
-    if(flag.on){
+void NoTimer(void) {
+    if (flag.on) {
         flag.on = false;
+        flag.secTick = false;
+        while (flag.secTick == false);
         RELAY_ON();
         flag.secTick = false;
         signal.blink_led = true;
         timerValue = 0;
-         return;
+        return;
     }
-    if (flag.secTick == true){
+    if (flag.secTick == true) {
         flag.secTick = false;
         timerValue++;
-        if(timerValue > 3)
+        if (timerValue > 2)
             DisplayValue(timerValue);
     }
 }
